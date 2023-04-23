9de2bf23976872878263d9c234e3bf9c6824dc77
Hossam Mahmoud <nhem318@gmail.com>
2023-04-23 11:30:56 +0200

    modify _printf.c file

--- _printf.c
+++ _printf.c
@@ -11,89 +11,61 @@
  * Return: the number of characters printed
  */
 
+#include <stdio.h>
+#include <stdarg.h>
+
 int _printf(const char *format, ...)
 {
-int i, j, len, special;
-char *value;
-special = 0;
-va_list arg;
+va_list args;
+int count = 0;
 
-len = 0;
-for (i = 0; format[i] != '\0'; i++)
-{
-if (format[i] == 'c' || format[i] == 's' || format[i] == '%')
-{
-special++;
-}
-len++;
-}
+va_start(args, format);
 
-if (special > 0)
+while (*format)
 {
-newOutput = (char *)malloc(len + 1);
-if (newOutput == NULL)
+if (*format == '%')
 {
-return -1;
-}
+format++;
 
-va_start(arg, special);
-
-j = 0;
-for (i = 0; format[i] != '\0'; i++)
-{
-if (format[i] == '%')
-{
-i++;
-switch (format[i])
+switch (*format)
 {
 case 'c':
-value = va_arg(arg, char *);
-newOutput[j] = *value;
-j++;
+{
+char c = (char)va_arg(args, int);
+putchar(c);
+count++;
 break;
+}
 case 's':
-value = va_arg(arg, char *);
-while (*value != '\0')
 {
-newOutput[j] = *value;
-j++;
-value++;
+const char *str = va_arg(args, const char *);
+while (*str)
+{
+putchar(*str++);
+count++;
 }
 break;
+}
 case '%':
-newOutput[j] = '%';
-j++;
+{
+putchar('%');
+count++;
 break;
+}
 default:
-free(newOutput);
 return -1;
 }
 }
 else
 {
-newOutput[j] = format[i];
-j++;
-}
-}
-
-va_end(arg);
-
-if (write(1, newOutput, j) == -1)
-{
-free(newOutput);
-return -1;
+putchar(*format);
+count++;
 }
 
-free(newOutput);
+format++;
+}
 
-return j;
-}
-else
-{
-if (write(1, format, len) == -1)
-{
-return -1;
-}
-return len;
-}
+va_end(args);
+
+return count;
 }
