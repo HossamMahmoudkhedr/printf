eac2adc65b728feee91a956a2fdbb8248eb20f8e
Hossam Mahmoud <nhem318@gmail.com>
2023-04-24 06:57:28 +0200

    modify printf.c file

--- _printf.c
+++ _printf.c
@@ -11,17 +11,17 @@
 int (*find_function(const char *format))(va_list)
 {
 unsigned int i = 0;
-code_f find_f[] =
-{
+code_f find_f[] = {
 {"c", printf_char},
 {"s", printf_string},
-{NULL, NULL}
-};
+{NULL, NULL}};
 
 while (find_f[i].sc)
 {
 if (find_f[i].sc[0] == (*format))
+{
 return (find_f[i].f);
+}
 i++;
 }
 return (NULL);
@@ -38,7 +38,9 @@ int (*f)(va_list);
 unsigned int i = 0, cprint = 0;
 
 if (format == NULL)
+{
 return (-1);
+}
 va_start(ap, format);
 while (format[i])
 {
@@ -49,7 +51,9 @@ cprint++;
 i++;
 }
 if (format[i] == '\0')
+{
 return (cprint);
+}
 f = find_function(&format[i + 1]);
 if (f != NULL)
 {
@@ -58,13 +62,19 @@ i += 2;
 continue;
 }
 if (!format[i + 1])
+{
 return (-1);
+}
 putchar(format[i]);
 cprint++;
 if (format[i + 1] == '%')
+{
 i += 2;
+}
 else
+{
 i++;
+}
 }
 va_end(ap);
 return (cprint);
