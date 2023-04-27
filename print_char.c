664b59275bf9f707c74b22cb95bc06f94eb2fae7
Hossam Mahmoud <nhem318@gmail.com>
2023-04-27 13:25:38 +0200

    Modify print_char.c file and print_string.c file

--- print_char.c
+++ print_char.c
@@ -1,27 +1,28 @@
+#include "main.h"
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
-#include "main.h"
 
 /**
- * print_char - prints a character
+ * print_char - Prints a character.
  *
- * @c: the character to be printed
- * Return: 1
+ * @c: The character to be printed.
+ *
+ * Return: Always success (1).
  */
-
 int print_char(va_list c)
 {
 unsigned int my_char;
+
 my_char = va_arg(c, int);
 putchar(my_char);
 return (1);
 }
 
 /**
- * print_percentage - prints the percentage mark
+ * print_percentage - Prints the percentage mark.
  *
- * Return: 1
+ * Return: Always success (1).
  */
 int print_percentage(void)
 {
--- print_string.c
+++ print_string.c
@@ -1,28 +1,29 @@
+#include "main.h"
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
-#include "main.h"
+
 /**
- * print_string - prints a string
+ * print_string - Prints a string.
  *
- * @s: the string to be printed
- * Return: the length of the string
+ * @s: The string to be printed.
+ *
+ * Return: The length of the string.
  */
 int print_string(va_list s)
 {
 char *my_string;
-
 int i = 0;
 
 my_string = va_arg(s, char *);
 if (my_string == NULL)
-{
 my_string = "(null)";
-}
+
 while (my_string[i])
 {
 putchar(my_string[i]);
 i++;
 }
+
 return (i);
 }
