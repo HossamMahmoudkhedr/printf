d8a33f95f95ca2c60d17ba28656796cb2a525d67
Hossam Mahmoud <nhem318@gmail.com>
2023-04-27 11:52:59 +0200

    modify the header file

--- main.h
+++ main.h
@@ -2,16 +2,17 @@
 #ifndef _PRINTF_H
 #define _PRINTF_H
 
-int _printf(const char *format, ...);
-int print_char(va_list c);
-int print_percentage(void);
-int print_string(va_list s);
-int print_dec(va_list d);
-int print_int(va_list i);
 
 typedef struct code_format
 {
 char *sc;
 int (*f)(va_list);
 } code_f;
+
+int _printf(const char *format, ...);
+int print_char(va_list c);
+int print_percentage(void);
+int print_string(va_list s);
+int print_dec(va_list d);
+int print_int(va_list i);
 #endif /* _PRINTF_H */
